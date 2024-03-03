db9f15d8d2d1f523db00cffc72ce18043d5a4e84
guiFerranti <guilhermecousseau@icloud.com>
2024-03-03 11:26:31 -0300

    creating basic project structure

---
+++ src/main/java/com/guilherme/AZShip/AzShipApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.AZShip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzShipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzShipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilherme/AZShip/AzShipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.AZShip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzShipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
