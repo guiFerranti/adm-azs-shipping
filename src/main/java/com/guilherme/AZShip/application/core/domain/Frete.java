c213034d48657e34bb4bcde5be61991df4e56a21
guiFerranti <guilhermecousseau@icloud.com>
2024-03-03 11:38:18 -0300

    adding base classes

---
+++ src/main/java/com/guilherme/AZShip/application/core/domain/Frete.java
@@ -0,0 +1,88 @@
+package com.guilherme.AZShip.application.core.domain;
+
+public class Frete {
+
+    public Frete() {}
+
+    public Frete(double peso, double altura, double largura, double comprimento, Address address, String cpf, String nome) {
+        this.peso = peso;
+        this.altura = altura;
+        this.largura = largura;
+        this.comprimento = comprimento;
+        this.address = address;
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+    private long id;
+
+    private double peso;
+
+    private double altura;
+
+    private double largura;
+
+    private double comprimento;
+
+    private Address address;
+
+    private String cpf;
+
+    private String nome;
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        this.largura = largura;
+    }
+
+    public double getComprimento() {
+        return comprimento;
+    }
+
+    public void setComprimento(double comprimento) {
+        this.comprimento = comprimento;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
